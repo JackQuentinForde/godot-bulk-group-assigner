fa6bd049d63db91e925aa80ca1acb905cc632ef4
JackQuentinForde <jackforde96@gmail.com>
2026-02-15 14:39:18 +0000

    minor improvements

--- Scripts/group_assigner.gd
+++ Scripts/group_assigner.gd
@@ -4,7 +4,7 @@ extends Node
 @export_file("*tscn") var sceneFile : String
 @export var nodePrefix : String = ""
 @export var groups : Array[String] = []
-@export var overwriteScene : bool = false
+@export var overwriteScene : bool = false ##When true, the original scene file will be overwritten. When false, a new scene file with the "_modified" suffix will be created in the same directory. Be very careful when enabling this option, as it may corrupt your scene if something goes wrong. If you are not using source control, it is highly recommended to keep this option disabled and review the changes in the newly created file before replacing the original scene.
 @export var runProcessing : bool = false : set = setRunProcessing
 
 func setRunProcessing(value : bool) -> void:
@@ -26,7 +26,7 @@ func processFile(prefix : String) -> void:
 	
 	var groupsPart = ""
 	if groups.size() > 0:
-		groupsPart = "groups=[\"" + groups[0] + "\""
+		groupsPart = " groups=[\"" + groups[0] + "\""
 
 		if groups.size() > 1:
 			for i in range(1, groups.size()):
@@ -45,7 +45,7 @@ func processFile(prefix : String) -> void:
 			var newLine = ""
 
 			if line.contains("groups=["):
-				var startIndex = line.find("groups=[")
+				var startIndex = line.find(" groups=[")
 				var endIndex = line.find("]", startIndex)
 
 				if endIndex == -1:
@@ -54,7 +54,10 @@ func processFile(prefix : String) -> void:
 					
 				newLine = line.substr(0, startIndex) + groupsPart + line.substr(endIndex + 1)
 			else:
-				newLine = line.rstrip("] ") + groupsPart + "]"
+				newLine = line.rstrip("]") + groupsPart + "]"
+
+			if line == newLine:
+				continue
 				
 			lines[i] = newLine
 			changesPending = true
